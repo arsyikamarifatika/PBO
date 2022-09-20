e6efe14aa1f8a658c27cbe73e1a3a980a3ef37c1
arsyikamarifatika <93310658+arsyikamarifatika@users.noreply.github.com>
2022-09-20 23:40:54 +0700

    Add files via upload

---
+++ Sesi1PBO.java
@@ -0,0 +1,11 @@
+//Nama  : Arsyika Ma'rifatika Suryana Putri
+//NIM   : 20210040063
+//Kelas TI 21 E
+// Sesi 1 PBO
+public class Sesi1PBO {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan meyenangkan");
+    }
+}
