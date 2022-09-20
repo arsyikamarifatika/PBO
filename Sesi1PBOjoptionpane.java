657cff110941240be060bea7797e94cf1eb06eb6
arsyikamarifatika <93310658+arsyikamarifatika@users.noreply.github.com>
2022-09-20 23:41:26 +0700

    Add files via upload

---
+++ Sesi1PBOjoptionpane.java
@@ -0,0 +1,12 @@
+//Nama  : Arsyika Ma'rifatika Suryana Putri
+//NIM   : 20210040063
+//Kelas TI 21 E
+// Sesi 1 PBO
+import javax.swing.*;
+public class Sesi1PBOjoptionpane {
+    public static void main(String[] args) {
+        String matkul; 
+        matkul = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar " + matkul + " sangat mudah");
+    }
+}
